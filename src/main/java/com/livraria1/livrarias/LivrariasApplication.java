6713aefacfac631384c7b9f4e190e2db80a9fce1
Maria Clara <mariaclariagomes@gmail.com>
2025-01-23 20:40:16 -0300

    first commit

---
+++ src/main/java/com/livraria1/livrarias/LivrariasApplication.java
@@ -0,0 +1,14 @@
+package com.livraria1.livrarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariasApplication.class, args);
+	}
+	
+
+}
